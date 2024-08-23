170dec0185d0864ffd53a266eb43051680bb5e99
kamal eth <kamalthedev7@gmail.com>
2024-08-23 14:26:23 +0000

    platform UI works smooth

---
+++ const/contractAddresses.ts
@@ -0,0 +1,14 @@
+import { BevmTestnet } from "@thirdweb-dev/chains";
+
+
+export const MARKETPLACE_ADDRESS = "";
+
+export const NFT_COLLECTION_ADDRESS = "";
+
+export const RENTNFT_COLLECTION_ADDRESS = "";
+
+export const NETWORK=  BevmTestnet;
+
+export const EXPLORER_URL = "https://scan-testnet.bevm.io/";
+
+export const GITHUB_URL = "https://github.com/0xsksingh/OrdinalsFi"
